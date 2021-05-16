30760fc9599e1a2c912facab408f574c3b9c3084
kamikaza36 <petar.klenovic@gmail.com>
2021-05-16 21:32:17 +0200

    added filters to api

--- orders/api.py
+++ orders/api.py
@@ -1,14 +1,21 @@
 from orders.models import Order, Machine, Client
 from rest_framework import viewsets, permissions
+from django_filters import rest_framework
 from .serializers import OrderSerializer, MachineSerializer, ClientSerializer
+from .filters import OrderFilter
 
 # Order Viewset
 class OrderViewSet(viewsets.ModelViewSet):
-    queryset = Order.objects.all()
+    ## queryset = Order.objects.select_related('machine')
+    filter_backends = (rest_framework.DjangoFilterBackend,)
+    serializer_class = OrderSerializer
+    filterset_class = OrderFilter
     permissions_classes = [
         permissions.AllowAny
     ]
-    serializer_class = OrderSerializer
+    def get_queryset(self):
+        b = Order.objects.prefetch_related('machine', 'client')
+        return b.select_related('machine')
 
 class MachineViewSet(viewsets.ModelViewSet):
     queryset = Machine.objects.all()
@@ -16,6 +23,7 @@ class MachineViewSet(viewsets.ModelViewSet):
         permissions.AllowAny
     ]
     serializer_class = MachineSerializer
+
 
 class ClientViewSet(viewsets.ModelViewSet):
     queryset = Client.objects.all()
