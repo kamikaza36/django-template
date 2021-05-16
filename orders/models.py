f8895c84ebbc7aefe16f75cefd369fe0c60e48ce
kamikaza36 <petar.klenovic@gmail.com>
2021-05-16 21:33:25 +0200

    added default values for some fields

--- orders/models.py
+++ orders/models.py
@@ -2,23 +2,17 @@ from django.db import models
 
 # Create your models here.
 class Machine(models.Model): 
-    machine_name = models.CharField(max_length=100)
+    machine_name = models.CharField(max_length=100, default='Nepoznata mašina')
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
-
-    def machine_name(self):
-        return(self.machine_name)
 
 class Client(models.Model):
-    client_name = models.CharField(max_length=100)
+    client_name = models.CharField(max_length=100, default='Nepoznati klijent')
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
 
-    def client_name(self):
-        return(self.client_name)
-
 class Order(models.Model):
-    order_name = models.CharField(max_length=100)
+    order_name = models.CharField(max_length=100, blank=True)
     order_quantity = models.IntegerField(blank=True)
     order_start_date = models.DateTimeField()
     order_end_date = models.DateTimeField()
