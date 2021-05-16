81e251f4b1b0f6c4edfcaa9fd9bac056d93cb27b
kamikaza36 <petar.klenovic@gmail.com>
2021-05-16 21:32:45 +0200

    added related tables to serializers

--- orders/serializers.py
+++ orders/serializers.py
@@ -2,20 +2,26 @@ from rest_framework import serializers, fields
 from orders.models import Order, Machine, Client
 
 # Order Serializer
-class OrderSerializer(serializers.ModelSerializer):
-    order_start_date = fields.DateTimeField(input_formats=['%Y-%m-%dT%H:%M:%S'])
-    order_end_date = fields.DateTimeField(input_formats=['%Y-%m-%dT%H:%M:%S'])
-
-    class Meta:
-        model = Order
-        fields = '__all__'
 
 class MachineSerializer(serializers.ModelSerializer):
+    machine_name = fields.CharField(max_length=100, allow_blank=True)
+
     class Meta:
         model = Machine
         fields = '__all__'
 
 class ClientSerializer(serializers.ModelSerializer):
+    client_name = serializers.CharField(max_length=100, allow_blank=True)
     class Meta:
         model = Client
+        fields = '__all__'
+
+class OrderSerializer(serializers.ModelSerializer):
+    order_start_date = fields.DateTimeField(input_formats=['%Y-%m-%dT%H:%M:%S'])
+    order_end_date = fields.DateTimeField(input_formats=['%Y-%m-%dT%H:%M:%S'])
+
+    machine = MachineSerializer()
+    client = ClientSerializer()
+    class Meta:
+        model = Order
         fields = '__all__'
