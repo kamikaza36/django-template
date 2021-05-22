4ba7056b089b12184e2375fc510b07bdd3448f78
kamikaza36 <petar.klenovic@gmail.com>
2021-05-22 20:56:26 +0200

    added frontend to django

--- mysite/settings.py
+++ mysite/settings.py
@@ -58,6 +58,7 @@ INSTALLED_APPS = [
     # Local Apps
     'users',
     'orders',
+    'frontend',
 ]
 
 MIDDLEWARE = [
@@ -140,6 +141,8 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
 
 STATIC_URL = '/static/'
+
+STATIC_ROOT = os.path.join(BASE_DIR, "static")
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
--- mysite/urls.py
+++ mysite/urls.py
@@ -1,8 +1,10 @@
+from frontend import urls
 from django.contrib import admin
 from django.urls import include, path
 
 urlpatterns = [
+    path('', include('frontend.urls')),
     path('admin/', admin.site.urls),
     path('api/v1/users/', include('users.urls')),
-    path('', include('orders.urls')),
+    path('orders/', include('orders.urls')),
 ]
